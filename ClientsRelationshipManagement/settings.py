4e0a65a2aebdffc1a1951b7240c6769273370557
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-29 16:40:16 +0200

    Update settings.py

--- ClientsRelationshipManagement/settings.py
+++ ClientsRelationshipManagement/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w647w44j)chrp8^+@_t$ce+naq_+u=3aay2ufh(+5b%yb+*(8n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
